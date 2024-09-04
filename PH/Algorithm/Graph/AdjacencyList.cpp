3f65ad042a160e63f42707fb1faa2de429800063
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2024-09-04 18:21:52 +0600

    feat: create AdjacencyList.cpp

---
+++ PH/Algorithm/Graph/AdjacencyList.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    const int N = 1e5+5;
+    // vector<int> adj[N];
+    vector<pair<int,int>> adj[N];
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=0; i<m; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    for(int i=1; i<=n; i++){
+        cout<<"List"<<" "<<i<<"- ";
+        for(auto j:adj[i]){
+            cout<<"("<<j.first<<","<<j.second<<")"<<",";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
